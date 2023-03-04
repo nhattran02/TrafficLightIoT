6809fb52fdc09c4108532fda0defc7d23cd6d09a
nhatVNU <tranminhnhat20022002@gmail.com>
2023-03-04 13:05:53 +0700

    Add uart commu

--- main/main.c
+++ main/main.c
@@ -4,6 +4,8 @@
 #include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
 #include "driver/gpio.h"
 #include "esp_err.h"
 #include "esp_log.h"
@@ -12,26 +14,35 @@
 #include "esp_spiffs.h"
 #include "st7735s.h"
 #include "fontx.h"
+#include "my_screen.h"
 
-/* This is defining the pins that the screen is connected to. */
-#define SCREEN_WIDTH 130
-#define SCREEN_HEIGHT 160
-#define OFFSET_X 0
-#define OFFSET_Y 0
-#define GPIO_MOSI 23
-#define GPIO_SCLK 19
-#define GPIO_CS 22
-#define GPIO_DC 21
-#define GPIO_RESET 18
-#define INTERVAL 200
-#define WAIT vTaskDelay(INTERVAL/portTICK_PERIOD_MS)
+#define LED 2 
+#define EX_UART_NUM UART_NUM_0
+#define PATTERN_CHR_NUM    (3)       
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
+TaskHandle_t TaskHandler_uart;
 
-static const char *TAG = "TrafficLightIoT";
-
-static void SPIFFS_Directory(char * path);
-void tft(void *pvParameters);
+void button_task(void *pvParameters);
+static void uart_event_task(void *pvParameters);
+bool isEqualString(char *str1, char *str2);
 
 void app_main(void){
+	gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+	uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    //Install UART driver, and get the queue.
+    uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
+    uart_param_config(EX_UART_NUM, &uart_config);
+    //Set UART pins (using UART0 default pins ie no changes.)
+    uart_set_pin(EX_UART_NUM, 1, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
 	esp_vfs_spiffs_conf_t conf = {
 		.base_path = "/spiffs",
@@ -39,63 +50,74 @@ void app_main(void){
 		.max_files = 10,
 		.format_if_mount_failed =true
 	};
-	esp_err_t ret =esp_vfs_spiffs_register(&conf);
+	esp_vfs_spiffs_register(&conf);
 
-	if (ret != ESP_OK) {
-		if (ret ==ESP_FAIL) {
-			ESP_LOGE(TAG, "Failed to mount or format filesystem");
-		} else if (ret== ESP_ERR_NOT_FOUND) {
-			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-		} else {
-			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
-		}
-		return;
+	xTaskCreate(&screen_task, "TFT Screen", 4096, NULL, 2, NULL);
+	//xTaskCreate(&button_task, "Button", 4096, NULL, 2, NULL);
+	xTaskCreate(&uart_event_task, "UART Task", 4096, NULL, 2, NULL);
+}
+void button_task(void *pvParameters){
+	while(1){
+		gpio_set_level(LED, 1);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+		gpio_set_level(LED, 0);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
 	}
-
-	size_t total = 0, used = 0;
-	ret = esp_spiffs_info(NULL, &total,&used);
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
-	} else {
-		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
-	}
-	xTaskCreate(tft, "TFT", 4096, NULL, 2, NULL);
+}
+bool isEqualString(char *str1, char *str2){
+    bool check=true;
+    for(int i=0;i<strlen(str2);i++){
+        if(str1[i]!=str2[i]){
+            check=false;
+            break;
+        }
+    }
+    return check;
 }
 
-void tft(void *pvParameters)
+static void uart_event_task(void *pvParameters)
 {
-	// set font file
-	FontxFile fx16[2];
-	FontxFile fx24[2];
-	FontxFile fx32[2];
-	InitFontx(fx16,"/spiffs/ILGH16XB.FNT",""); // 8x16Dot Gothic
-	InitFontx(fx24,"/spiffs/ILGH24XB.FNT",""); // 12x24Dot Gothic
-	InitFontx(fx32,"/spiffs/ILGH32XB.FNT",""); // 16x32Dot Gothic
-	ST7735_t dev;
-	spi_master_init(&dev, GPIO_MOSI, GPIO_SCLK, GPIO_CS, GPIO_DC, GPIO_RESET);
-	lcdInit(&dev, SCREEN_WIDTH, SCREEN_HEIGHT, OFFSET_X, OFFSET_Y);
-	//Effect
- 	
-	while (1) {
-		lcdFillScreen(&dev, GREEN);
-		WAIT;
-		lcdDrawFillRect(&dev, 0, 0, 20, 150, RED);
-		WAIT;
-	}
+    uart_event_t event;
+    size_t buffered_size;
+    char* dtmp = (char*) malloc(RD_BUF_SIZE);
+    while(1) {
+        //Waiting for UART event.
+        printf("\n----------CONTROLL TRAFFIC LIGHT VIA TERMINAL----------\n");
+        printf("1. Set TimeLight:     !SETTIME! \n");
+        printf("2. Manual Adjust:     !ADJ! \n");
+        printf("3. Slow Mode:         !SLOW! \n");
+        printf("4. Show Current Time: !SHOW! \n");
+
+        if(xQueueReceive(uart0_queue, (void * )&event, (TickType_t)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            //if(event.data)
+            switch(event.type) {
+                case UART_DATA:
+                    uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
+                    printf("%s", dtmp);
+                    if(isEqualString(dtmp, "!SETTIME!")){
+                        gpio_set_level(LED, 1);
+
+                    }else if(isEqualString(dtmp, "!ADJ!")){
+                        gpio_set_level(LED, 0);
+
+                    }else if(isEqualString(dtmp, "!SLOW!")){
+                        for(int i=0;i<5;i++){
+                            gpio_set_level(LED, 1);
+                            vTaskDelay(500/portTICK_PERIOD_MS);
+                            gpio_set_level(LED, 0);
+                            vTaskDelay(500/portTICK_PERIOD_MS);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
 }
 
-/**
- * It prints the contents of the directory.
- * 
- * @param path The path to the directory to list.
- */
-static void SPIFFS_Directory(char * path) {
-	DIR* dir = opendir(path);
-	assert(dir != NULL);
-	while (true) {
-		struct dirent*pe = readdir(dir);
-		if (!pe) break;
-		ESP_LOGI(TAG,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
-	}
-	closedir(dir);
-}
+
